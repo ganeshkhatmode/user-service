23ccf744806b8b28ae8c94b49e8ec725f9aa2bd9
ganeshkhatmode <ganeshkhatmode@gmail.com>
2023-04-16 22:20:09 +0530

    add repository class

--- src/main/java/com/gk/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/gk/userservice/service/impl/UserServiceImpl.java
@@ -1,23 +1,28 @@
 package com.gk.userservice.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gk.userservice.model.User;
+import com.gk.userservice.repository.UserRepository;
 import com.gk.userservice.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public User save(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return userRepository.save(user);
 	}
 
 	@Override
 	public User findByUserId(long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return userRepository.findById(userId).get();
 	}
 
 }
